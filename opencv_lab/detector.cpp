dba5b095987294daa086d0da23b7839e9275ae32
Doryeong Park <zzada0520@gmail.com>
2017-03-06 16:56:07 +0900

    Detector experiment code added

--- opencv_lab/detector.cpp
+++ opencv_lab/detector.cpp
@@ -61,7 +61,7 @@ public:
 
 	bool is_valid() {
 		if (first_x == -1 || first_y == -1
-			|| second_x == -1 || second_y == -1 ) {
+			|| second_x == -1 || second_y == -1) {
 			return false;
 		}
 		return true;
@@ -71,17 +71,17 @@ public:
 		if (first_x == -1 || first_y == -1
 			|| second_x == -1 || second_y == -1)
 			return Mat();
-		
+
 		auto rect = Rect(Point(first_x, first_y), Point(second_x, second_y));
 		first_x = -1; first_y = -1; second_x = -1; second_y = -1;
-		
+
 		return cframe(rect);
 	}
 
 	Mat& get_cframe() {
 		return cframe;
 	}
-		
+
 	Point get_first_point() {
 		return Point(first_x, first_y);
 	}
@@ -94,12 +94,10 @@ public:
 
 void on_mouse(int callback_event, int x, int y, int flags, void* param);
 void expand_rect(Rect& rect, const int& pixel) noexcept;
-void save_objects(Mat& cframe, Mat& cframe_gray, Mat& binary, vector<Rect>& final_rects) noexcept;
-void classify(Mat& cframe, Mat& cframe_gray, Mat& binary, vector<Rect>& final_rects) noexcept;
-void train(Mat& data);
+void display_keypoints(Mat& cframe, Mat& cframe_gray, Mat& binary, vector<Rect>& final_rects) noexcept;
 
 void on_mouse(int callback_event, int x, int y, int flags, void* param) {
-	
+
 	Cropper* cropper = (Cropper*)param;
 
 	if (callback_event == CV_EVENT_LBUTTONDOWN) {
@@ -131,7 +129,7 @@ void on_mouse(int callback_event, int x, int y, int flags, void* param) {
 }
 
 void expand_rect(Rect& rect, const int& pixel) noexcept {
-	
+
 	int x1 = rect.x;
 	int y1 = rect.y;
 	int x2 = rect.x + rect.width;
@@ -153,126 +151,6 @@ void expand_rect(Rect& rect, const int& pixel) noexcept {
 	rect.height = y2 - y1;
 }
 
-void save_objects(Mat& cframe, Mat& cframe_gray, Mat& binary, vector<Rect>& final_rects) noexcept{
-
-	for (Rect& rect : final_rects) {
-		int width = rect.width;
-		int height = rect.height;
-		float ratio = (float)(DATA_HEIGHT / 2) / (float)height;
-
-		int modified_width = (int)((float)rect.width * ratio);
-		int modified_height = DATA_HEIGHT / 2;
-		
-		if (modified_width > DATA_WIDTH)
-			continue;
-
-		Mat object_cframe = cframe_gray(rect);
-		Mat object_binary = binary(rect);
-
-		resize(object_cframe, object_cframe, Size(modified_width, modified_height), 0, 0, CV_INTER_CUBIC);
-		resize(object_binary, object_binary, Size(modified_width, modified_height), 0, 0, CV_INTER_AREA);
-		
-		threshold(object_binary, object_binary, 0, 1, CV_THRESH_BINARY);
-		object_cframe = object_cframe.mul(object_binary);
-
-		Mat background = Mat(DATA_HEIGHT, DATA_WIDTH, CV_8UC1);
-		background = Scalar::all(0);
-	
-		Mat result = background.clone();
-		
-		Rect roi;
-		roi.width = modified_width;
-		roi.height = modified_height;
-		roi.x = (DATA_WIDTH - modified_width) / 2;
-		roi.y = (DATA_HEIGHT - modified_height) / 2;
-		
-		Mat background_roi = background(roi);
-
-		addWeighted(background_roi, 0, object_cframe, 1, 0.0, result(roi));
-		
-		imwrite(to_string(++matarial_count) + ".jpg", result);
-	}
-
-}
-
-void classify(Mat& cframe, Mat& cframe_gray, Mat& binary, vector<Rect>& final_rects) noexcept {
-	
-	for (auto iter = final_rects.begin(); iter != final_rects.end();) {
-		int width = iter->width;
-		int height = iter->height;
-		float ratio = (float)(DATA_HEIGHT / 2) / (float)height;
-
-		int modified_width = (int)((float)iter->width * ratio);
-		int modified_height = DATA_HEIGHT / 2;
-
-		if (modified_width > DATA_WIDTH) {
-			++iter;
-			continue;
-		}
-
-		Mat object_cframe = cframe_gray(*iter);
-		Mat object_binary = binary(*iter);
-
-		resize(object_cframe, object_cframe, Size(modified_width, modified_height), 0, 0, CV_INTER_CUBIC);
-		resize(object_binary, object_binary, Size(modified_width, modified_height), 0, 0, CV_INTER_AREA);
-
-		threshold(object_binary, object_binary, 0, 1, CV_THRESH_BINARY);
-		object_cframe = object_cframe.mul(object_binary);
-
-		Mat background = Mat(DATA_HEIGHT, DATA_WIDTH, CV_8UC1);
-		background = Scalar::all(0);
-
-		Mat result = background.clone();
-
-		Rect roi;
-		roi.width = modified_width;
-		roi.height = modified_height;
-		roi.x = (DATA_WIDTH - modified_width) / 2;
-		roi.y = (DATA_HEIGHT - modified_height) / 2;
-
-		Mat background_roi = background(roi);
-
-		addWeighted(background_roi, 0, object_cframe, 1, 0.0, result(roi));
-
-		//Check with classifier
-		keypoints.clear();
-		detector->detect(result, keypoints);
-		extractor->compute(result, keypoints, descriptors);
-		descriptors = descriptors.reshape(1, 1);
-		
-		if (descriptors.cols != classifier->getVarCount()) {
-			iter = final_rects.erase(iter);
-			continue;
-		}
-
-		float is_matching = classifier->predict(descriptors.reshape(1,1));
-
-		Mat result_clone = result.clone();
-
-		Mat cframe_clone = cframe.clone();
-		rectangle(cframe_clone, *iter, Scalar(0, 0, 255));
-
-		if (is_matching != 1) {
-			iter = final_rects.erase(iter);
-			putText(result_clone, "NOT CORRECT", Point(20, 20), 1, 0.8, Scalar(255));
-		}
-		else {
-			putText(result_clone, "CORRECT", Point(20, 20), 1, 0.8, Scalar(255));
-			++iter;
-		}
-
-		imshow("Input", cframe_clone);
-		imshow("Result", result_clone);
-
-  		char ch = waitKey(10);
-
- 		while ((ch = waitKey(10)) != 32 && ch != 27);
-
-		if (ch == 32) { continue; }
-
-   	}
-	cvDestroyWindow("Result");
-}
 
 int main() {
 
@@ -283,17 +161,12 @@ init:
 	//Routine input file name
 	string file_name = "";
 	cout << "Please input file name: ";
-	cin >> file_name;	
+	cin >> file_name;
 	if (!ifstream(file_name).good()) {
 		cout << "File doesn't exist" << endl;
 		goto init;
 	}
 
-	if (!ifstream("classifier.yml")) {
-		cout << "Classifier doesn't exist" << endl;
-		goto init;
-	}
-	
 	//Load classifier
 	classifier = Algorithm::load<SVM>("classifier.yml");
 
@@ -317,7 +190,7 @@ init:
 
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
-	
+
 	//Background Setting 
 	if (ifstream("Background.jpg").good()) {
 		Mat loaded_image = imread("Background.jpg", CV_LOAD_IMAGE_GRAYSCALE);
@@ -334,14 +207,14 @@ init:
 
 	//Initialize Cropper 
 	Cropper cropper;
-	
+
 	//Playing video file
 	while (true) {
 		camera.read(cframe);
 
 		if (cframe.empty())
 			break;
-		
+
 		//Mouse event callback setting 
 		cropper.update_frame(cframe);
 		setMouseCallback("Input", on_mouse, &cropper);
@@ -356,7 +229,7 @@ init:
 
 		morphologyEx(binary, binary, MORPH_OPEN, noise_remover);
 		morphologyEx(binary, binary, MORPH_CLOSE, expander);
-		
+
 		findContours(
 			binary, contours, hierarchy,
 			CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point{ 0, 0 });
@@ -398,7 +271,7 @@ init:
 						//Remove Noise of Binary image(Small Rect)
 						expand_rect(*another_rect, 2);
 						threshold(binary(*another_rect), binary(*another_rect), 0, 0, CV_THRESH_BINARY);
-						
+
 						//Remove Rect object 
 						bounded_rects.erase(another_rect);
 						another_rect = bounded_rects.begin();
@@ -411,24 +284,15 @@ init:
 		for (Rect& final_rect : bounded_rects) {
 			expand_rect(final_rect, 4);
 		}
-	
-		putText(cframe, "Count: " + to_string(bounded_rects.size()),
-			Point{ 15, 15 },
-			3,
-			0.5,
-			Scalar{ 255, 0, 0 });
 
 		imshow("Input", cframe);
 		imshow("Background", background);
-		imshow("Binary", binary);
 		
 		char ch = waitKey(10);
 
 		//Space key
-		if(ch == 32){                
+		if (ch == 32) {
 			//Save all objects detected as file
-			save_objects(cframe, cframe_gray, binary, bounded_rects);
-			classify(cframe, cframe_gray, binary, bounded_rects);
 			while ((ch = waitKey(10)) != 32 && ch != 27);
 			if (ch == 27) break;
 		}
@@ -442,6 +306,5 @@ init:
 	}
 
 	destroyAllWindows();
-	imwrite("Background.jpg", accumulator);
 }
 
