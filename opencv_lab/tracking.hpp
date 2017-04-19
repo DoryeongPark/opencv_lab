5ac8b44420d1ed08ebdb014d7da7c18471646079
Doryeong Park <zzada0520@gmail.com>
2017-04-19 15:33:14 +0900

    Inital commit for Tracking classes

---
+++ opencv_lab/tracking.hpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+/*
+	Author		:	Doryeong Park
+	Date		:	18. 4. 2017	
+	Description	:	Instantiates & Manages tracking images   
+*/
+
+namespace tracking {
+
+	
+	
+	/*
+		Expresses one object image bound and Information of 
+	*/
+	class TrackingObject {
+
+		
+
+	};
+
+
+	/*
+		Manager class for all tracking objects
+	*/
+	class TrackingObjectPool {
+
+
+
+
+	};
+	
+}
